b507bc2c38bc1b56b67212456ac8d4b31a549e5d
LuigiToniolo <luigi.toniolo@domvsit.com.br>
2021-09-24 14:45:37 -0300

    Upando código com página de cadastro e imagens responsivas

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,21 @@
 <template>
-
-  <div>
-    <h1>{{ titulo }}</h1>
-
-    <ul>
-
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
-      </li>
-
-    </ul>
-
+  <div class="corpo">
+    <router-view></router-view>
   </div>
 </template>
 
 
 <script>
+
 export default {
-
-  data() {
-    return {
-
-        titulo: 'Alurapic',
-        fotos: [
-          {
-            url: 'https://carroscomcamanzi.com.br/wp-content/uploads/2019/12/Lamborghini-Lambo-Gran-Turismo-Concept-2019-1-1333x1000.jpg',
-            titulo: 'carro'
-          },
-          {
-            url: 'https://carroscomcamanzi.com.br/wp-content/uploads/2019/12/Lamborghini-Lambo-Gran-Turismo-Concept-2019-1-1333x1000.jpg',
-            titulo: 'lambo'
-          }
-        ]
-    }
-  }
 }
 
 </script>
 
 <style>
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 </style>
