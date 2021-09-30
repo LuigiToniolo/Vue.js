91dc1c65d0d67f6c00fd14c13ab76f5d5d153bc2
LuigiToniolo <luigi.toniolo@domvsit.com.br>
2021-09-30 00:06:59 -0300

    Finalizando o codigo

--- src/components/shared/botao/Botao.vue
+++ src/components/shared/botao/Botao.vue
@@ -49,25 +49,8 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="sass">
 
-    $cor: firebrick;
+    @import './Botao.scss';
 
-    .botao {
-        display: inline-block;
-        padding: 10px;
-        border-radius: 3px;
-        margin: 10px;
-        font-size: 1.2em;
-    }
-
-    .botao-perigo {
-        background: $cor;
-        color: white;
-    }
-
-    .botao-padrao {
-        background: darkcyan;
-        color: white;
-    }
 </style>
